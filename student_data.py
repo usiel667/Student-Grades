63b4e325b37c1e79ef96f1e732e4d6fb073d6d21
usiel667 <tom_evans1@me.com>
2025-05-28 11:31:36 -0600

    Update student_data.py
    Added student id to list

--- student_data.py
+++ student_data.py
@@ -1,10 +1,9 @@
 # This file contains the initial student data for use in other scripts.
 
 student_data = [
-    ["Allen", "english", 67.5],
-    ["Karren", "math", 79.9],
-    ["Travis", "physics", 95.5],
-    ["Travers", "science", 87.5],
-    ["Arken", "physics", 76]
+    ["Allen", 123456, "english", 67.5],
+    ["Karren", 234567, "math", 79.9],
+    ["Travis", 345678, "physics", 95.5],
+    ["Travers", 456789, "science", 87.5],
+    ["Arken", 567890, "physics", 76]
 ]
-
