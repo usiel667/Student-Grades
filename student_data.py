aeb006a12b9e120782a54dd33c05e3a3e8428545
usiel667 <tom_evans1@me.com>
2025-06-09 22:21:10 -0600

    no comment yet

--- student_data.py
+++ student_data.py
@@ -1,7 +1,6 @@
 # This file contains the initial student data for use in other scripts.
 
 student_data = [
-<<<<<<< HEAD
     ["Allen", "english", 67.5],
     ["Karren", "math", 79.9],
     ["Travis", "physics", 95.5],
@@ -32,11 +31,3 @@ list_b = [["Allen", "english", 67.5],
           ]
        
        
-=======
-    ["Allen", 123456, "english", 67.5],
-    ["Karren", 234567, "math", 79.9],
-    ["Travis", 345678, "physics", 95.5],
-    ["Travers", 456789, "science", 87.5],
-    ["Arken", 567890, "physics", 76]
-]
->>>>>>> bd6c0af9357f13fe67b13c805fe204e3f4cc146f
